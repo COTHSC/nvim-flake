a5a6c18da1651c3fb31e497c7193bb3b2608b0fe
jcts <jcts@jctss-MacBook-Pro.local>
2023-05-01 23:57:11 +0200

    initial commit

---
+++ plugins.nix
@@ -0,0 +1,25 @@
+#plugins.nix
+{pkgs}:
+with pkgs.vimPlugins; [
+  telescope-nvim
+  toggleterm-nvim # better terminal management
+  (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars)) # better code coloring
+  playground # treesitter playground
+nvim-treesitter-textobjects # jump around and select based on syntax (class, function, etc.)
+  nvim-treesitter-context # keep current block header (func defn or whatever) on first line
+  nvim-cmp # generic autocompleter
+  cmp-nvim-lsp # use lsp as source for completions
+  cmp-nvim-lua # makes vim config editing better with completions
+  cmp-buffer # any text in open buffers
+  cmp-path # complete paths
+  cmp-cmdline # completing in :commands
+  cmp-emoji # complete :emojis:
+  cmp-nvim-lsp-signature-help # help complete function call by showing args
+  cmp-npm # complete node packages in package.json
+  nvim-autopairs # balances parens as you type
+  nvim-ts-autotag # balance or rename html
+  rose-pine
+  lsp-zero-nvim
+  undotree
+  fugitive
+]
