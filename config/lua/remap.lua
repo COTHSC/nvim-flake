434c92c193100b22a8785aa631e137590b3081a8
jcts <jcts@jctss-mbp-1.home>
2023-05-10 09:05:28 +0200

    remaps for navigating buffers

--- config/lua/remap.lua
+++ config/lua/remap.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 
 vim.keymap.set("n", "<leader>gp", vim.cmd.ChatGPT)
+vim.api.nvim_set_keymap('n', '<C-j>', '<cmd>wincmd j<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>wincmd k<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>wincmd l<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<cmd>wincmd h<CR>', { noremap = true, silent = true })
