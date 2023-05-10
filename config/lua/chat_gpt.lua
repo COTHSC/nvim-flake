539a9a9ead9834a2cbffcd1edea42380c172552c
jcts <jcts@jctss-mbp-1.home>
2023-05-10 08:55:15 +0200

    add gpt I guess

--- config/lua/chat_gpt.lua
+++ config/lua/chat_gpt.lua
@@ -1,1 +1,3 @@
+require("telescope")
+require("plenary")
 require("chatgpt").setup()
--- config/lua/remap.lua
+++ config/lua/remap.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>", {})
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
+
+vim.keymap.set("n", "<leader>gp", vim.cmd.ChatGPT)
