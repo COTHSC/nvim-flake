659bac0e4403326cf9dcb27f0456f3446000a5d6
jcts <jcts@pop-os.lan>
2025-10-07 13:52:37 +0200

    Clearer signs of compiler errors now working properly

--- config/lua/lsp.lua
+++ config/lua/lsp.lua
@@ -18,14 +18,13 @@ require('mason-lspconfig').setup({
     },
 })
 
-local signs = {
-  Error = "❌",
-  Warn = "⚠️ ",
-  Hint = "💡",
-  Info = "ℹ️ "
-}
-
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "✘",
+      [vim.diagnostic.severity.WARN] = "▲",
+      [vim.diagnostic.severity.HINT] = "⚑",
+      [vim.diagnostic.severity.INFO] = "»",
+    }
+  }
+})
--- packages/myNeovim.nix
+++ packages/myNeovim.nix
@@ -1,11 +1,14 @@
 { pkgs }:
 let
-  customRC = import ../config/default.nix { inherit pkgs; };
-  plugins = import ../plugins.nix { inherit pkgs; };
+    customRC = import ../config/default.nix { inherit pkgs; };
+    plugins = import ../plugins.nix { inherit pkgs; };
 in
-pkgs.neovim.override {
-  configure = {
-    inherit customRC;
-    packages.all.start = plugins;
-  };
-}
+    pkgs.neovim.override {
+        configure = {
+            inherit customRC;
+            packages.all.start = plugins;
+        };
+        extraPackages = with pkgs; [
+            ripgrep
+        ];
+    }
